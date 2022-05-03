28eaeeb70ecc03f49ca0055f618574c65986f039
Davis Peck <Dpeck1997@gmail.com>
2022-05-03 13:58:52 -0700

    trying to find formats for pdf

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -31,12 +31,26 @@ export default {
 <template>
   <div id="data">
     <h1>This is a demo of converting HTML to PDF</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum saepe, nihil ipsa ab rem, natus atque odio vero
-      ea voluptatum vitae repellendus eligendi commodi eius officiis eaque molestias sint! Et?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum saepe, nihil ipsa ab rem, natus atque odio vero</p>
+
+  <p>ea voluptatum vitae repellendus eligendi commodi eius officiis eaque molestias sint! Et?</p>
 
   </div>
   <button v-on:click="htmlToPdf">Download File As PDF</button>
 </template>
 
 <style>
+
+
+/* h1 {
+  color: red;
+}
+
+p {
+  font-family: sans-serif;
+  text-align: left;
+  font-size: small;
+} */
+
+
 </style>
