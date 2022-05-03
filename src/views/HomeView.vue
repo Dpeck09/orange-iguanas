e240bcb48b3147e8c856c0892f15318abb61c810
Davis Peck <Dpeck1997@gmail.com>
2022-05-03 11:00:01 -0700

    getting a pdf button to work

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,42 @@
+/* eslint-disable */
+
 <script>
-  export default {
-    data: function () {
-      return {
-        message: "Welcome to Vue.js!",
-      };
-    },
-    created: function () {},
-    methods: {},
-  };
+import jsPDF from "jspdf";
+// import html2canvas from "html2canvas";
+// import dompurify from "dompurify";
+// import $ from "jquery";
+export default {
+  el: '#app',
+  data() {
+    return {
+      message: 'Hi my first vue code pen!!!'
+    }
+  },
+  methods: {
+    //jspdf does not include the bootstrap style layout
+    htmlToPdf: function () {
+      var doc = new jsPDF();
+      doc.html(document.getElementById("data"),
+        {
+          callback: function (doc) {
+            doc.save("example.pdf");
+          }
+        }
+      )
+    }
+  }
+};
 </script>
 
 <template>
-  <div class="home">
-    <h1>{{ message }}</h1>
+  <div id="data">
+    <h1>This is a demo of converting HTML to PDF</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum saepe, nihil ipsa ab rem, natus atque odio vero
+      ea voluptatum vitae repellendus eligendi commodi eius officiis eaque molestias sint! Et?</p>
+
   </div>
+  <button v-on:click="htmlToPdf">Download File As PDF</button>
 </template>
 
-<style></style>
+<style>
+</style>
